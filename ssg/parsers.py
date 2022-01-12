29379f35445037a202e078b50f2f56a0503fc0a1
mwang213 <wang.michael213@gmail.com>
2022-01-12 03:06:05 -0600

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,24 +10,24 @@ class Parser:
     def valid_extension(self,extension):
         return extension in self.extensions
     
-    def parse(path: Path, source: Path, dest: Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
-    def read(path: Path):
+    def read(self, path: Path):
         with open(path) as file:
             return file.read()
     
-    def write(path:Path, dest: Path, content,ext = ".html"):
+    def write(self, path:Path, dest: Path, content,ext = ".html"):
         full_path=dest / path.with_suffix(ext).name
         with open(full_path) as file:
             file.write(content)
     
-    def copy(path: Path, source: Path, dest: Path):
+    def copy(self, path: Path, source: Path, dest: Path):
         shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
     
     extensions: List[str] = [".jpg",".png", ".gif", ".css",".html"]
     
-    def parse(path: Path, source: Path, dest: Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         super.copy(path, source, dest)
